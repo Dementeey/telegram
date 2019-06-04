a276a5c91c21de15cabd8abc33e90ca78c54d385
Anton Loyko <anton.loyko@technorely.com>
2019-06-04 14:22:33 +0300

    Update: now

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const router = express.Router();
 const app = express();
 
 const TOKEN = 'министерство_не_ваших_собачих_дел';
-const PORT = parseInt(process.env.PORT, 10) || 80;
+// const PORT = parseInt(process.env.PORT, 10) || 9000;
 
 const testController = (req, res) => {
   const { body } = req;
-  console.log('=?=body=?>', body)
+  console.log('=?=body=?>', body);
 
   const payload = {
     status: 'ok',
@@ -23,14 +23,14 @@ const testController = (req, res) => {
   return res.status(201).send(payload);
 };
 
-router.get('/', testController);
+app.get('/', testController);
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(router);
+// app.use(router);
 app.use((req, res, next) => {
   console.log('Time: ', Date.now());
   next();
 });
 
-app.listen(PORT, () => console.log('HTTP server listening on port %s', PORT));
+// app.listen(PORT, () => console.log('HTTP server listening on port %s', PORT));
