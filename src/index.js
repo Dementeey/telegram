1bf1eb08f4ce019c2291992119fd6260d4ac9e1d
Anton Loyko <anton.loyko@technorely.com>
2019-06-05 19:13:43 +0300

    Update:

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
+const http = require('http');
+const https = require('https');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 const router = express.Router();
 const app = express();
-const sendMessage = require('./sendMessage');
 
 const TOKEN = 'министерство_не_ваших_собачих_дел';
 const PORT = parseInt(process.env.PORT, 10) || 9000;
+
+const options = {
+  host: 'jsonplaceholder.typicode.com',
+  port: 80,
+  path: '/posts/1',
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
+const sendMessage = data => {
+  console.log('============data=====================');
+  console.log(data);
+  console.log('====================================');
+
+  const port = options.port === 443 ? https : http;
+  const req = port.request(options, res => {
+    console.log('============response===============');
+    console.log(`${options.host} : ${res.statusCode}`);
+    console.log('==============END==================');
+  });
+
+  req.on('error', error => {
+    console.log('=========REQUEST ERROR=============');
+    console.log(error);
+    console.log('=============END===================');
+  });
+
+  req.end();
+};
 
 const testController = (req, res) => {
   const { body } = req;
